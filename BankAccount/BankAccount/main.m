3fdbfe6ddac9e4425e065e62afc430b1fcb28ad6
Shena Yoshida <sheyoshida@gmail.com>
2015-06-21 15:10:19 -0400

    first commit 4

---
+++ BankAccount/BankAccount/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  BankAccount
+//
+//  Created by Shena Yoshida on 2015-06-21.
+//  Copyright (c) 2015 Shena Yoshida. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
